fe8819fcf50673fd730c8d29298437766739d7c5
Matheus Pellegrini <matheuspellegrinini@gmail.com>
2022-08-23 20:32:27 -0300

    First but not the last

---
+++ android/app/src/main/kotlin/com/example/app_tarkov/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_tarkov
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/feature/ItemsList.dart
@@ -0,0 +1,53 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import '../settings/settings_view.dart';
+import 'item.dart';
+import 'itemDetail.dart';
+
+/// Displays a list of SampleItems.
+class SampleItemListView extends StatelessWidget {
+  const SampleItemListView({
+    Key? key,
+    this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
+  }) : super(key: key);
+
+  static const routeName = '/';
+
+  final List<SampleItem> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Items'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        restorationId: 'sampleItemListView',
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = items[index];
+
+          return ListTile(
+              title: Text('SampleItem ${item.id}'),
+              leading: const CircleAvatar(
+                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+              ),
+              onTap: () {
+                Navigator.restorablePushNamed(
+                  context,
+                  SampleItemDetailsView.routeName,
+                );
+              });
+        },
+      ),
+    );
+  }
+}
